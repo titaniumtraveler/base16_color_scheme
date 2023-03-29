9430126cfb137b48c15ea4d3cedd573258540332
Jon Heinritz <jon.heinritz@protonmail.com>
2023-03-29 17:03:01 +0200

    Reimplement parsing using nom

--- src/template/color_field.rs
+++ src/template/color_field.rs
@@ -1,4 +1,10 @@
 use lazy_static::lazy_static;
+use nom::{
+    branch::alt,
+    bytes::complete::{tag, take},
+    sequence::tuple,
+    Finish, IResult,
+};
 use regex::Regex;
 use serde::{Deserialize, Serialize};
 use std::str::FromStr;
@@ -17,32 +23,32 @@ impl FromStr for ColorField {
     type Err = ColorFieldError;
 
     fn from_str(input: &str) -> Result<Self, Self::Err> {
-        lazy_static! {
-            static ref RE: Regex =
-                Regex::new(r"^base([0-9A-F]{2})-(hex|rgb|dec)(?:-(r|g|b|bgr))?$").unwrap();
-        }
+        let (input, (number, output_format, output_variant)) = parse_field(input)
+            .finish()
+            .map_err(|_| ColorFieldError::FieldFormat)?;
 
-        let capture = RE.captures(input).ok_or(ColorFieldError::FieldFormat)?;
-        let number = &capture[1];
+        if !input.is_empty() {
+            return Err(ColorFieldError::FieldFormat);
+        }
 
         let mut hex = [0u8];
         hex::decode_to_slice(number, &mut hex).map_err(|_| ColorFieldError::NumberFormat)?;
         let [number] = hex;
 
-        let output_format = match &capture[2] {
+        let output_format = match output_format {
             "hex" => OutputFormat::Hex,
             "rgb" => OutputFormat::Rgb,
             "dec" => OutputFormat::Dec,
             _ => return Err(ColorFieldError::OutputFormat),
         };
 
-        let output_variant = match capture.get(3).map(|m| m.as_str()) {
-            Some("r") => OutputVariant::R,
-            Some("b") => OutputVariant::G,
-            Some("g") => OutputVariant::B,
-            Some("bgr") => OutputVariant::Bgr,
-            Some(_) => return Err(ColorFieldError::OutputVariant),
-            None => OutputVariant::Rgb,
+        let output_variant = match output_variant {
+            "" => OutputVariant::Rgb,
+            "r" => OutputVariant::R,
+            "b" => OutputVariant::G,
+            "g" => OutputVariant::B,
+            "bgr" => OutputVariant::Bgr,
+            _ => return Err(ColorFieldError::OutputVariant),
         };
 
         Ok(Self {
@@ -53,6 +59,18 @@ impl FromStr for ColorField {
             },
         })
     }
+}
+
+fn parse_field(input: &str) -> IResult<&str, (&str, &str, &str)> {
+    let (input, (_, number, _, format, _, variant)) = tuple((
+        tag("base"),
+        take(2usize),
+        tag("-"),
+        alt((tag("hex"), tag("rgb"), tag("dec"))),
+        tag("-"),
+        alt((tag(""), tag("r"), tag("g"), tag("b"), tag("bgr"))),
+    ))(input)?;
+    Ok((input, (number, format, variant)))
 }
 
 #[derive(Debug, Clone)]
