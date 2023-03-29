886adcc2e1da1bfb80f8e07280416393e4ead27c
Jon Heinritz <jon.heinritz@protonmail.com>
2023-03-29 20:12:52 +0200

    Restructure nom parser

--- src/template/color_field.rs
+++ src/template/color_field.rs
@@ -1,11 +1,11 @@
-use lazy_static::lazy_static;
+use hex::FromHexError;
 use nom::{
     branch::alt,
     bytes::complete::{tag, take},
+    combinator::map_res,
     sequence::tuple,
-    Finish, IResult,
+    Finish, IResult, Parser,
 };
-use regex::Regex;
 use serde::{Deserialize, Serialize};
 use std::str::FromStr;
 
@@ -23,60 +23,65 @@ impl FromStr for ColorField {
     type Err = ColorFieldError;
 
     fn from_str(input: &str) -> Result<Self, Self::Err> {
-        let (input, (number, output_format, output_variant)) = parse_field(input)
-            .finish()
-            .map_err(|_| ColorFieldError::FieldFormat)?;
+        let (input, (number, format)) = parse_field(input).finish().map_err(|_| ColorFieldError)?;
 
         if !input.is_empty() {
-            return Err(ColorFieldError::FieldFormat);
+            return Err(ColorFieldError);
         }
 
-        let mut hex = [0u8];
-        hex::decode_to_slice(number, &mut hex).map_err(|_| ColorFieldError::NumberFormat)?;
-        let [number] = hex;
-
-        let output_format = match output_format {
-            "hex" => OutputFormat::Hex,
-            "rgb" => OutputFormat::Rgb,
-            "dec" => OutputFormat::Dec,
-            _ => return Err(ColorFieldError::OutputFormat),
-        };
-
-        let output_variant = match output_variant {
-            "" => OutputVariant::Rgb,
-            "r" => OutputVariant::R,
-            "b" => OutputVariant::G,
-            "g" => OutputVariant::B,
-            "bgr" => OutputVariant::Bgr,
-            _ => return Err(ColorFieldError::OutputVariant),
-        };
-
-        Ok(Self {
-            number,
-            format: Format {
-                output_format,
-                output_variant,
-            },
-        })
+        Ok(Self { number, format })
     }
 }
 
-fn parse_field(input: &str) -> IResult<&str, (&str, &str, &str)> {
-    let (input, (_, number, _, format, _, variant)) = tuple((
+fn parse_field(input: &str) -> IResult<&str, (u8, Format)> {
+    use OutputFormat::{Dec, Hex, Rgb};
+    let (input, (_, number, _, format)) = tuple((
         tag("base"),
-        take(2usize),
+        map_res(take(2usize), |input| -> Result<u8, FromHexError> {
+            let mut hex = [0u8];
+            hex::decode_to_slice(input, &mut hex)?;
+            let [number] = hex;
+
+            Ok(number)
+        }),
         tag("-"),
-        alt((tag("hex"), tag("rgb"), tag("dec"))),
-        tag("-"),
-        alt((tag(""), tag("r"), tag("g"), tag("b"), tag("bgr"))),
+        alt((
+            tag("hex")
+                .and(alt((
+                    tag("").map(|_| OutputVariant::Rgb),
+                    tag("-r").map(|_| OutputVariant::R),
+                    tag("-g").map(|_| OutputVariant::G),
+                    tag("-b").map(|_| OutputVariant::B),
+                    tag("-bgr").map(|_| OutputVariant::Bgr),
+                )))
+                .map(|(_, output_variant)| Format {
+                    output_format: Hex,
+                    output_variant,
+                }),
+            tag("rgb")
+                .and(alt((
+                    tag("-r").map(|_| OutputVariant::R),
+                    tag("-g").map(|_| OutputVariant::G),
+                    tag("-b").map(|_| OutputVariant::B),
+                )))
+                .map(|(_, output_variant)| Format {
+                    output_format: Rgb,
+                    output_variant,
+                }),
+            tag("dec")
+                .and(alt((
+                    tag("-r").map(|_| OutputVariant::R),
+                    tag("-g").map(|_| OutputVariant::G),
+                    tag("-b").map(|_| OutputVariant::B),
+                )))
+                .map(|(_, output_variant)| Format {
+                    output_format: Dec,
+                    output_variant,
+                }),
+        )),
     ))(input)?;
-    Ok((input, (number, format, variant)))
+    Ok((input, (number, format)))
 }
 
 #[derive(Debug, Clone)]
-pub enum ColorFieldError {
-    FieldFormat,
-    NumberFormat,
-    OutputFormat,
-    OutputVariant,
-}
+pub struct ColorFieldError;
