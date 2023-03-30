2eead55771a2f49b6770e78c454953b389aaf28a
Jon Heinritz <jon.heinritz@protonmail.com>
2023-03-30 12:41:45 +0200

    Add basic usage example

---
+++ examples/basic_usage.rs
@@ -0,0 +1,12 @@
+use base16_color_scheme::{Scheme, Template};
+use std::fs::read_to_string;
+
+fn main() {
+    let template_str = read_to_string("examples/template.mustache").unwrap();
+    let scheme_str = read_to_string("examples/scheme.yml").unwrap();
+
+    let template = Template::new(template_str).unwrap();
+    let scheme: Scheme = serde_yaml::from_str(&scheme_str).unwrap();
+
+    println!("{}", template.render(&scheme));
+}
