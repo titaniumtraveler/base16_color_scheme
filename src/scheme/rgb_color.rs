a441bc68f58fd4eaaf3c07b1a2b7c7bec7c661b4
Jon Heinritz <jon.heinritz@protonmail.com>
2023-04-13 01:44:34 +0200

    Add documentation for RgbColor

--- src/scheme/rgb_color.rs
+++ src/scheme/rgb_color.rs
@@ -4,6 +4,20 @@ pub use formatter::RgbColorFormatter;
 
 mod formatter;
 
+/// type representing a hex rgb color, which implements [`Serialize`](serde::Serialize)/[`Deserialze`](serde::Deserialize)
+///
+/// it uses [`hex`] for the implementation so it's deserialization is case insensitive and it's
+/// serialization is lower case by default.
+///
+/// # Examples
+///
+/// ```rust
+/// use base16_color_scheme::scheme::RgbColor;
+///
+/// let color: RgbColor = serde_yaml::from_str("7CaFc2").unwrap();
+/// assert_eq!(color, RgbColor([0x7c, 0xaf, 0xc2]));
+/// assert_eq!(serde_yaml::to_string(&color).unwrap(), "7cafc2\n");
+/// ```
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct RgbColor(pub [u8; 3]);
 
